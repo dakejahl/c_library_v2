91e1cf37197d1e911c25dc9464d2481e1ca6e93c
Jacob Dahl <dahl.jakejacob@gmail.com>
2025-08-26 13:34:54 -0800

    build all

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH -5845947717413548453
+#define MAVLINK_COMMON_XML_HASH -2307875848591127385
 
 #ifdef __cplusplus
 extern "C" {
